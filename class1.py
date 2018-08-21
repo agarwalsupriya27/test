e0f17e92893ebad19e5fb0476db6686fbb3980b8
agarwalsupriya27 <37625807+agarwalsupriya27@users.noreply.github.com>
2018-08-21 12:50:05 +0530

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,6 @@
+print ('i m a python program')
+value_input = input("Enter a radius:")
+radius = float(value_input)
+area = 3.14 * radius ** 2
+print ("the area of the circle with radius" +value_input+ "is:")
+print (area)
